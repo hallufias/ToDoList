1a95ff4473bf3a568be90bf9f1f68123f9653075
Saifullah Nazar <saidergamer@yahoo.co.uk>
2020-04-17 17:52:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoItemRecyclerView : RecyclerView
+    private lateinit var recyclerAdapter: TodoItemsAdapter
+    private lateinit var recyclerLayoutManager : RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        todoItemsList.add(TodoItem("Buy Groceries"))
+        todoItemsList.add(TodoItem("Do Laundry", true))
+        todoItemsList.add(TodoItem("Play Guiter", false))
+
+        todoItemRecyclerView = findViewById(R.id.todo_item_recycler_view)
+
+        recyclerLayoutManager = LinearLayoutManager(this) //view from top to bottom layout
+        recyclerAdapter = TodoItemsAdapter(todoItemsList, this)
+
+        todoItemRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = recyclerLayoutManager
+            adapter = recyclerAdapter
+        }
+    }
+
+    public fun navToAddItemPage(view : View){
+        val intent : Intent = Intent(this, AddItemActivity::class.java)
+        startActivity(intent)
+    }
+}

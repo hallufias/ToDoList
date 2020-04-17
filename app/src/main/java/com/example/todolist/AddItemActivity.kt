2a4889f92b7d4695d692bf049461592f00db1588
Saifullah Nazar <saidergamer@yahoo.co.uk>
2020-04-17 20:19:24 +0800

    add button intent added. mainacticity cancel button intent added. additemactivity

--- app/src/main/java/com/example/todolist/AddItemActivity.kt
+++ app/src/main/java/com/example/todolist/AddItemActivity.kt
@@ -1,12 +1,26 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_item.*
 
 class AddItemActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
+
+
+    }
+
+    fun cancelItemAction (view: View){
+        val cancelIntent = Intent(this, MainActivity::class.java)
+        startActivity(cancelIntent)
+    }
+
+    fun saveItemAction (view: View){
+
     }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -10,31 +10,31 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var todoItemRecyclerView : RecyclerView
-    private lateinit var recyclerAdapter: TodoItemsAdapter
+//    private lateinit var recyclerAdapter: TodoItemsAdapter
     private lateinit var recyclerLayoutManager : RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        todoItemsList.add(TodoItem("Buy Groceries"))
-        todoItemsList.add(TodoItem("Do Laundry", true))
-        todoItemsList.add(TodoItem("Play Guiter", false))
+//        todoItemsList.add(TodoItem("Buy Groceries"))
+//        todoItemsList.add(TodoItem("Do Laundry", true))
+//        todoItemsList.add(TodoItem("Play Guiter", false))
 
-        todoItemRecyclerView = findViewById(R.id.todo_item_recycler_view)
-
-        recyclerLayoutManager = LinearLayoutManager(this) //view from top to bottom layout
-        recyclerAdapter = TodoItemsAdapter(todoItemsList, this)
-
-        todoItemRecyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = recyclerLayoutManager
-            adapter = recyclerAdapter
-        }
+//        todoItemRecyclerView = findViewById(R.id.rvTodoList)
+//
+//        recyclerLayoutManager = LinearLayoutManager(this) //view from top to bottom layout
+//        recyclerAdapter = TodoItemsAdapter(todoItemsList, this)
+//
+//        todoItemRecyclerView.apply {
+//            setHasFixedSize(true)
+//            layoutManager = recyclerLayoutManager
+//            adapter = recyclerAdapter
+//        }
     }
 
-    public fun navToAddItemPage(view : View){
-        val intent : Intent = Intent(this, AddItemActivity::class.java)
+    fun navToAddItemPage(view : View){
+        val intent = Intent(this, AddItemActivity::class.java)
         startActivity(intent)
     }
 }
